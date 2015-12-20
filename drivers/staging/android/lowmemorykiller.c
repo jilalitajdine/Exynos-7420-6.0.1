1d6f77972cfd48710527d85df8446a907d57a104
UpInTheAir <upintheair.xda@gmail.com>
2015-12-20 17:11:28 +0700

    Adaptive LMK: set vmpressure_file_min value to 180 MB
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -101,7 +101,7 @@ module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
  * 90-94. Usually this is a pseudo minfree value, higher than the
  * highest configured value in minfree array.
  */
-static int vmpressure_file_min;
+static int vmpressure_file_min = 46080;
 module_param_named(vmpressure_file_min, vmpressure_file_min, int,
 	S_IRUGO | S_IWUSR);
 
