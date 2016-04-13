760c4d450314568439b9f7fef60de29bbb43df26
UpInTheAir <upintheair.xda@gmail.com>
2016-04-14 06:24:38 +0700

    BUILD: initial N920P support
    - Add stock N920PVPU2BPC3 ramdisk & dt.img
    - Add N920P to build scripts
    - Create build & output directories
    - Merge dts files from N920PVPU2BPC3 source
    - Add stock N920P defconfig from N920PVPU2BPC3 source
    
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- env_setup.sh
+++ env_setup.sh
@@ -51,6 +51,7 @@ else
         echo ""
         echo "You need to define your device target!"
         echo "example: build_kernel.sh N920C"
+        echo "example: build_kernel.sh N920P"
         echo "example: build_kernel.sh N9200"
         echo "example: build_kernel.sh G928C"
         exit 1
@@ -75,6 +76,11 @@ if [ "$TARGET" = "N920C" ] ; then
 	else
 		export KERNEL_CONFIG="SkyHigh_N920C_no-audio_defconfig";
 	fi;
+fi;
+
+# SM-N920 P (Sprint)
+if [ "$TARGET" = "N920P" ] ; then
+	export KERNEL_CONFIG="SkyHigh_N920P_defconfig";
 fi;
 
 # SM-N9200 HK
