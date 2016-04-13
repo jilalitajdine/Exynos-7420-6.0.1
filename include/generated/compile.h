db3881687402f8323f949c3563f9b9ca6da79daa
UpInTheAir <upintheair.xda@gmail.com>
2016-04-13 18:25:19 +0700

    N9200ZHU2BPC4
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Apr 7 21:17:21 KST 2016"
+#define LINUX_COMPILE_BY "dao.zheng"
+#define LINUX_COMPILE_HOST "R0709-09"
+#define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (GCC) "
